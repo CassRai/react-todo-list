d6f5e68c60b94a1469c40a22506cad9004cc260b
Cassandra <cassandraedwards2024@gmail.com>
2024-06-28 13:21:36 +0100

    added Footer and changed array to fragment

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@
 import Header from './layout/Header';
 import Checkbox from './layout/Checkbox';
 import { my_list } from './data/to-do-list';
+import Footer from './layout/Footer';
 
 import './App.css'
 
@@ -14,14 +15,15 @@ function App() {
   
 
   return (
-    [<Header />,
+    // <> </> = fragment - an array alternative 
+    <> 
+    <Header /> 
     <form action="">
       {/** {} within here to show an object and that its not html */}
       {my_list.map(({name, value, done}) => (<Checkbox key={name+value} name={name} value={value!} done={done} />)) }
     </form>
-    ]
-
-
+    <Footer /> 
+    </>
   );
 }
 
---
+++ src/layout/Footer.tsx
@@ -0,0 +1,5 @@
+export default function Footer(){
+    return <footer>
+        <p>Made by Cassandra Edwards</p>
+        </footer>
+}
