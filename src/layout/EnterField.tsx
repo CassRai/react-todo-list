cc6445ca5b3c97edf5296d21d2e1763397a2546c
Cassandra <cassandraedwards2024@gmail.com>
2024-07-01 11:07:12 +0100

    adding button to form

--- src/layout/EnterField.tsx
+++ src/layout/EnterField.tsx
@@ -1,8 +1,23 @@
 export default function EnterField(){
+
+    function handleSubmit(e){
+        // Prevent the browser from reloading the page
+        e.preventDefault();
+
+        // Read the form data
+        const form = e.target;
+        const formData = new FormData(form);
+    }
+
+
+
     return <>
+    <form method="post" onSubmit={handleSubmit}>
     <label>
-        Text input: <input name="myInput" />
+        Add an activity:  <input name="myInput" />
     </label>
+    <div id="button_section"><button type="button">Add to list</button></div>
+    </form>
     </>
     
 }
