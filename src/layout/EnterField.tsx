a7d32c9401c14b8705f61a14a99b1d7103161993
Cassandra <cassandraedwards2024@gmail.com>
2024-07-01 10:04:25 +0100

    playing around with input in react

--- src/layout/EnterField.tsx
+++ src/layout/EnterField.tsx
@@ -1,7 +1,8 @@
 export default function EnterField(){
     return <>
-    <div><label id="main_label" htmlFor="name"> What do you have to do? </label></div>
-    <input type="text" id="user_input"/>
+    <label>
+        Text input: <input name="myInput" />
+    </label>
     </>
     
 }
