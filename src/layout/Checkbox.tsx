8c8fcbe7b4072e469095b79d26d3897d003c5f66
Cassandra <cassandraedwards2024@gmail.com>
2024-07-02 10:36:37 +0100

    successfully set up checkbox state to implement checking off feature

--- src/layout/Checkbox.tsx
+++ src/layout/Checkbox.tsx
@@ -10,11 +10,22 @@ JSX = html within javascript
 
 */
 
+import { useEffect, useState } from 'react';
+
 export default function Checkbox({name, value}:{name:string, value?:string}){
     const IdValue = `checkbox-${value ?? name}`;
+    const [isChecked, setIsChecked] = useState(false);
+
+    const checkHandler = () => {
+        /*set isChecked to the opposite of what it currently is?
+        so if isChecked = true set it to false
+        else isChecked = false set it to True
+        */
+        setIsChecked(!isChecked)
+      }
 
     return [ 
-        <input type="checkbox" id={IdValue} name={name} value={value}/>, 
+        <input type="checkbox" id={IdValue} name={name} value={value} checked={isChecked} onChange={checkHandler}/>, 
         "   ",
         <label htmlFor={IdValue}>{name}</label>
     ]
